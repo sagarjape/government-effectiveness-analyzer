a5a7f59d1f310fb87b76de99ae92e3481535f15d
sagarjape <sagarjape29@gmail.com>
2019-08-13 18:17:57 +0530

    Add files via upload

---
+++ db.sql
@@ -0,0 +1,4 @@
+﻿Create table worlddata( year integer, country varchar(100), voice_accountability float, politicalstability_voilence float, governmenteffectiveness float, regulatory_quality float, rule_of_law float, controlofcurroption float)
+select * from worlddata
+copy worlddata(year, country, voice_accountability, politicalstability_voilence, governmenteffectiveness,regulatory_quality, rule_of_law, controlofcurroption) from 'C:\worlddata.csv' using DELIMITERS ',' csv header encoding 'windows-1251';
+drop table worlddata
